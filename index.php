856f87634d2dd7890d897883205e73f6e09ade76
Thiago Duarte <thiago-duarte@outlook.com>
2015-10-13 23:24:07 -0300

    "Alteração na estrutura do projeto"

--- index.php
+++ index.php
@@ -1,47 +1,48 @@
 <!DOCTYPE html>
 <html lang="pt">	
 <head>	
-		
         <meta charset="utf-8">
         <link rel="stylesheet" href="css/materialize.css">
         <link rel="stylesheet" href="css/index.css">
-                    	<!-- JS  -->
+        <!-- JS  -->
         <script src="js/jquery.js"></script>
         <script src="js/materialize.js"></script>
-
         <title>Bem vindo</title>
-
-	</head>
+</head>
         <body>
                 <div class="container-fluid">
-                        
                         <div class="row">
-                        </div>
-                        <div class="row">
-                                <h1>Login</h1>
                                 <form action="usuarios.php" method="POST" >
+                                        <h1 >Login</h1>
+                                         <?php 
+                                               if (isset($_GET['err'])){
+                                                  echo '<p ><b>senha inválida</b></p>';     
+                                               }else
+                                               {
+                                                 echo  '<p>Bem vindo</p>';
+                                               }                                               
+                                         ?>
+               
                                         <div class="row">
                                                 <div class="input-field col s12">
-                                                <input id="Usuario" type="text" name="usuario" >
-                                                <label for="Usuario">Usuario</label>
-                                        </div>
-                                        <div class="row">
-                                                <div class="input-field col s12">
+                                                        <input id="Usuario" type="text" name="usuario" >
+                                                        <label for="Usuario">Usuario</label>
+                                                </div>
+                                                <div class="row">
+                                                        <div class="input-field col s12">
                                                         <input id="senha" type="password" name="senha"  >
                                                         <label for="senha">senha</label>
                                                 </div>
                                         </div>
                                         <div class="row ">
-                                                <div>
+                                                <div class="input-field col s12">
                                                         <input class="btn" type="submit" value="Log in" >                                
                                                 </div>
                                         </div>
                                 </form>
                         </div>                        
                 </div>
-        <footer>
-        </footer>
-        <script src="js/usuarios.js"></script>
-        </body>
-	
+                <footer></footer>
+                <script src="js/usuarios.js"></script>
+        </body>	
 </html>
--- view/inserirUsuario.php
+++ view/inserirUsuario.php
@@ -1,38 +1,12 @@
-
-<!--
-  
-  RG, CPF, Email, Nome, Mãe, End, Cidade, Estado, Login e Senha.
-  
--->
-
 <?php 
   
-  $lista_cidades = array();
-  $novaCidade  = array();
-  $novaCidade['id'] = '1';
-  $novaCidade['nome'] = 'Porto Alegre';
-  $lista_cidades[] = $novaCidade;
+  $lista_cidades = GetCidades();;
+  $lista_Estados = GetUfs();
  
-  $novaCidade  = array();
-  $novaCidade['id'] = '2';
-  $novaCidade['nome'] = 'Gravatai';
-  $lista_cidades[] = $novaCidade;
  
-  $novaCidade  = array();
-  $novaCidade['id'] = '3';
-  $novaCidade['nome'] = 'Alvorada';
-  $lista_cidades[] = $novaCidade;
-  
-  
-  $lista_estados = array();
-  $novoEstado  = array();
-  $novoEstado['id'] = '1';
-  $novoEstado['nome'] = 'Rio Grande do Sul';
-  $lista_Estados[] = $novoEstado;
-  
 ?>
 	<div class="row s12">
-    <form class="col s12">
+    <form class="col s12" action="./controller/validarUsuario.php" >
       <div class="row">
         <div class="input-field col s6">
           <input  id="Nome" type="text" class="validate" name="nome">
@@ -42,14 +16,12 @@
           <input id="email" type="email" class="validate" name="email">
           <label for="email">Email</label>
         </div>
-
       </div>
       <div class="row">
         <div class="input-field col s4">
           <input  id="fone" type="text" class="validate">
           <label >Fone</label>
         </div>
-        
         <div class="input-field col s4">
           <input  id="rg" type="text" class="validate">
           <label >RG</label>
@@ -59,20 +31,17 @@
           <label >CPF</label>
         </div>
       </div>
-           <div class="row">
+      <div class="row">
         <div class="input-field col s6">
-          <input id="pai" type="email" class="validate">
+          <input id="pai" type="text" class="validate">
           <label for="pai">Pai</label>
         </div>
         <div class="input-field col s6">
           <input  id="mae" type="text" class="validate">
           <label >Mae</label>
         </div>
-         
       </div>
-
       <div class="row">
-        
         <div class="input-field col s6">
           <input  id="endereco" type="text" class="validate">
           <label >Endereço</label>
@@ -96,23 +65,15 @@
                       <?php  echo 	$estado['nome'];; ?></option>
                     <?php endforeach; ?>								  
                   </select>				    		
-          </div>
-         
+          </div>       
       </div>
-      
       <div class="row ">
         <div class="input-field col s6">
-          <button class="btn waves-effect waves-light " type="submit" name="action">Cadastrar
-            <i class="material-icons right">send</i>
+          <button class="btn waves-effect waves-light " type="submit" >Cadastrar
+            <i class="material-icons right"></i>
           </button>
           </div>
        </div>
-      
     </form>
   </div>
-  <?php 
-    
- 
-    
-  ?>
-    
+  
