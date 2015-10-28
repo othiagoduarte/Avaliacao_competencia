6437737f5082d3bbde8db7558e0fe0483652617a
Thiago Duarte <thiago-duarte@outlook.com>
2015-10-28 01:14:35 -0200

    "Alteração no radapé e na tela de login"

--- application/views/login.php
+++ application/views/login.php
@@ -1,24 +1,25 @@
 <div>
-	<div class="container-fluid">
-		<div class="row ">
-			<form action="login/logar" method="POST" >
+	<div class="container-fluid ">
+		<div class="row login col s12 m5">
+			<form action="login/logar" method="POST"  >
 				<h1 >Login</h1>
 				<?php echo isset($_GET['err'])?'<p><b class"red">senha inválida</b></p>':'<p>Bem vindo</p>';?>
 				<div class="row">
-					<div class="input-field col s12">
+					<div class="input-field ">
 						<input id="Usuario" type="text" name="usuario" >
 						<label for="Usuario">usuario</label>
 					</div>
-					<div class="row">
-						<div class="input-field col s12">
-							<input id="senha" type="password" name="senha"  >
-							<label for="senha">senha</label>
-						</div>
+				</div>
+				<div class="row">
+					<div class="input-field ">
+						<input id="senha" type="password" name="senha"  >
+						<label for="senha">senha</label>
 					</div>
-					<div class="row ">
-						<div class="input-field col s12">
+				</div>
+				<div class="row ">
+						<div class="input-field ">
 							<input class="btn" type="submit" value="Log in" >                                
-					</div>
+						</div>
 				</div>
 			</form>
 		</div>                        
